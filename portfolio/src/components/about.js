aac8975c245b63ac1ae259515fea20a8cbc42fe5
joemokhtarali@gmail.com <joemokhtarali@gmail.com>
2020-06-11 20:43:22 -0400

    responsive meta tags

--- portfolio/src/components/about.js
+++ portfolio/src/components/about.js
@@ -9,49 +9,54 @@ import Academy from '../style/modern-Academy.jpeg'
 export default function About() {
     return (
         <div className='about-me'>
-            <NavBar /> 
-
-            <h1 className='about-me-header'>About Me</h1>
-            <div className='first'>
+            <NavBar />
+            <div className='headlights'>
+                <div className='divider'></div>
                 <div className='profile-img'>
                     <img src={Me} className='profile_image' />
                 </div>
+                <div className='divider'></div>
+            </div>
+            <div className='first'>
+                {/* <div className='profile-img'>
+                    <img src={Me} className='profile_image' />
+                </div> */}
 
                 <div className='info-container'>
                     <p className='my-info' >
-                        Experienced Ruby on Rails and JavaScript based programmer with a background in Accounting and Customer Service.<br/><hr/>
-                        Passionate about breaking down computers both software and hardware into pieces, and putting them back together like puzzles.<br/><hr/>
-                        Skilled at problem solving, debugging, creative design and helping tech companies develop and improve the quality and style of their applications. <br/><hr/>
+                        Experienced Ruby on Rails and JavaScript based programmer with a background in Accounting and Customer Service.<br /><hr />
+                        Passionate about breaking down computers both software and hardware into pieces, and putting them back together like puzzles.<br /><hr />
+                        Skilled at problem solving, debugging, creative design and helping tech companies develop and improve the quality and style of their applications. <br /><hr />
                         If I am not programming, I am Traveling the world and blogging about it on <br />
                         <a href='https://passportshub.website/' target='_blank' className='my-info-website'>WWW.PassportsHub.com</a>
                     </p>
-                 </div>
-                 </div>
+                </div>
+            </div>
 
-                 <h1 className='about-me-header' style={{ 'marginTop': '70px' }}>Education</h1>
-                 <div style={{display: 'flex'}}>
-                <div className='second' style={{'marginLeft': '20%'}}>
+            <h1 className='about-me-header' style={{ 'marginTop': '70px' }}>Education</h1>
+            <div style={{ display: 'flex' }}>
+                <div className='second' style={{ 'marginLeft': '20%' }}>
                     <div className='flatiron-img'>
                         <img src={Flatiron} className='flatiron_image' />
                     </div>
                     <div className='flatiron-container'>
                         <h3 className='my-info-1' >Flatiron School - NYC</h3>
                         <h4 className='my-info-2' >Software Engingeering January 2020 - April 2020 - NYC</h4>
-                  </div>
-                  </div> {/* second */}
+                    </div>
+                </div> {/* second */}
 
-                  <div className='second'>
+                <div className='second'>
                     <div className='flatiron-img'>
                         <img src={Academy} className='flatiron_image' />
                     </div>
                     <div className='flatiron-container'>
                         <h3 className='my-info-1' >Modern Academy - Egypt</h3>
                         <h4 className='my-info-2' >Bachelor in Business Administration and Management June 2010 - Cairo</h4>
-                  </div>
-                  </div>
-                 </div> 
-        
-                    
-        </div> 
-    ) 
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+    )
 }
