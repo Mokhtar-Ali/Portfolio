56838ee299d25fae1d5769e09997ee337f9756fe
joemokhtarali@gmail.com <joemokhtarali@gmail.com>
2020-09-16 14:21:09 +0200

    modified Blogs page

--- portfolio/src/Blogs/Blogs.js
+++ portfolio/src/Blogs/Blogs.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import NavBar from '../components/navbar'
+import NavBar from '../components/navbar2'
 import Tech from './tech.png'
 import Travel from './travel.png'
 import '../style/blogs.css'
@@ -16,17 +16,18 @@ class Blogs extends React.Component {
                     <div className='tech'>
                         <h2>Technical Blogs</h2>
                         <ul>
-                            <a href='https://medium.com/@joemokhtarali/recursion-in-ruby-cli-project-cd12d9e56e3e' target='_blank'><li>Recursion and CLI</li></a>
-                            <a href='https://medium.com/@joemokhtarali/state-props-intro-to-react-41d878a01864' target='_blank'><li>State & Props in React</li></a>
-                            <a href='https://medium.com/@joemokhtarali/portfolio-ebdcab1b0e8c' target='_blank'><li>Create a Stylish Portfolio</li></a>
-                            <a href='https://medium.com/@joemokhtarali/javascript-consoles-and-debugging-skills-33cc6b22de15' target='_blank'><li>Javascript Debugging Skills</li></a>
-                            <a href='https://medium.com/@joemokhtarali/python3-vs-ruby-vs-javascript-329ba2fd3b0b' target='_blank'><li>Python vs Ruby vs Javascript</li></a>
-                            <a href='https://medium.com/@joemokhtarali/adding-a-new-domain-to-namecheap-fe4b8c8a78df' target='_blank'><li>Host domains using NameCheap</li></a>
-                            <a href='https://medium.com/@joemokhtarali/impress-interviewers-with-recursion-c10e8286894' target='_blank'><li>Impress Interviewers with Recursion</li></a>
-                            <a href='https://medium.com/@joemokhtarali/how-to-improve-your-pair-programming-skills-c03a474d2de' target='_blank'><li>How to improve your Pair Programming skills</li></a>
-                            <a href='https://medium.com/@joemokhtarali/intervals-part-3-create-games-using-react-redux-88d3479c7fe7' target='_blank'><li>Create Games using React-Redux3 Set & Clear Intervals </li></a>
-                            <a href='https://medium.com/@joemokhtarali/create-games-using-react-redux-e68bdd3ac298' target='_blank'><li>Create Games using React-Redux1 Reducers & Action Creators </li></a>
-                            <a href='https://medium.com/@joemokhtarali/part-2-create-games-using-react-redux-class-vs-functional-component-d53afc99232a' target='_blank'><li>Create Games using React-Redux2 Class vs Functional Components</li></a>
+                            <a href='https://medium.com/@joemokhtarali/nothing-is-such-a-complicated-algorithms-d7dce5980505' target='_blank'><li>Algorithms Are Fun!</li></a>
+                            <a href='https://medium.com/@joemokhtarali/recursion-in-ruby-cli-project-cd12d9e56e3e' target='_blank'><li>Recursion and CLI.</li></a>
+                            <a href='https://medium.com/@joemokhtarali/state-props-intro-to-react-41d878a01864' target='_blank'><li>State & Props in React.</li></a>
+                            <a href='https://medium.com/@joemokhtarali/portfolio-ebdcab1b0e8c' target='_blank'><li>Create a Stylish Portfolio.</li></a>
+                            <a href='https://medium.com/@joemokhtarali/javascript-consoles-and-debugging-skills-33cc6b22de15' target='_blank'><li>Javascript Debugging Skills.</li></a>
+                            <a href='https://medium.com/@joemokhtarali/python3-vs-ruby-vs-javascript-329ba2fd3b0b' target='_blank'><li>Python vs Ruby vs Javascript.</li></a>
+                            <a href='https://medium.com/@joemokhtarali/adding-a-new-domain-to-namecheap-fe4b8c8a78df' target='_blank'><li>Host domains using NameCheap.</li></a>
+                            <a href='https://medium.com/@joemokhtarali/impress-interviewers-with-recursion-c10e8286894' target='_blank'><li>Impress Interviewers with Recursion.</li></a>
+                            <a href='https://medium.com/@joemokhtarali/how-to-improve-your-pair-programming-skills-c03a474d2de' target='_blank'><li>How to improve your Pair Programming skills.</li></a>
+                            <a href='https://medium.com/@joemokhtarali/intervals-part-3-create-games-using-react-redux-88d3479c7fe7' target='_blank'><li>Create Games using React-Redux3 Set & Clear Intervals. </li></a>
+                            <a href='https://medium.com/@joemokhtarali/create-games-using-react-redux-e68bdd3ac298' target='_blank'><li>Create Games using React-Redux1 Reducers & Action Creators. </li></a>
+                            <a href='https://medium.com/@joemokhtarali/part-2-create-games-using-react-redux-class-vs-functional-component-d53afc99232a' target='_blank'><li>Create Games using React-Redux2 Class vs Functional Components.</li></a>
                         </ul>
                     </div>
                     <div className='travel'>
@@ -37,8 +38,8 @@ class Blogs extends React.Component {
                             <a href='https://passportshub.website/ecuador' target='_blank'><li>Ecuador, Quito</li></a>
                             <a href='https://passportshub.website/ecuador' target='_blank'><li>Ecuador, Baños</li></a>
                             <a href='https://passportshub.website/colombia' target='_blank'><li>Colombia, Medelin</li></a>
-                            <a href='https://passportshub.website/colombia' target='_blank'><li>Colombia, Cartagena</li></a>
-                            <a href='https://passportshub.website/colombia' target='_blank'><li>Colombia, Santa Marta</li></a>
+                            {/* <a href='https://passportshub.website/colombia' target='_blank'><li>Colombia, Cartagena</li></a>
+                            <a href='https://passportshub.website/colombia' target='_blank'><li>Colombia, Santa Marta</li></a> */}
                         </ul>
                     </div>
                 </div>
---
+++ portfolio/src/components/navbar2.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import '../style/navbar2.css' 
+// import Link from 'react-router-dom'
+
+
+export default function NavBar() { 
+ 
+    return (
+        <header className='toolbar'>
+            <nav className='toolbar_navigation'>
+                {/* <h3 className='nav-developer' >Developer</h3> */}
+
+                <div className='spacer'></div>
+
+                <div className='toolbar_navigation-items'>
+                    <ul >
+                        <li className='nav2-li'><a className='navbar-li' href='/' > Home </a></li>
+                        <li className='nav2-li'><a className='navbar-li' href='/projects'> Projects </a></li>
+                        <li className='nav2-li'><a className='navbar-li' href='/blogs'> Blogs </a></li>
+                        <li className='nav2-li'><a className='navbar-li' href='/about'> About </a></li>
+                        <li className='nav2-li'><a className='navbar-li' href='/contact'> Contact </a></li>
+                    </ul>
+                </div>
+                <div className='spacer'></div>
+            </nav>
+        </header>
+    );
+}
+
