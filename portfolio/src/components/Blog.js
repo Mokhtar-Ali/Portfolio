59f92aa5d0aebdc6a59d54efe56374b10b0ec210
joemokhtarali@gmail.com <joemokhtarali@gmail.com>
2020-11-25 19:35:58 -0500

    blogs in an array

---
+++ portfolio/src/components/Blog.js
@@ -0,0 +1,70 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
+import ButtonBase from "@material-ui/core/ButtonBase";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    margin: "auto",
+    maxWidth: 500,
+    marginBottom: "10px",
+  },
+  image: {
+    width: 128,
+    height: 128,
+  },
+  img: {
+    margin: "auto",
+    display: "block",
+    maxWidth: "100%",
+    maxHeight: "100%",
+  },
+}));
+
+export default function Blog(props) {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <Paper className={classes.paper}>
+        <Grid container spacing={2}>
+          <Grid item>
+            <ButtonBase className={classes.image}>
+              <img
+                className={classes.img}
+                alt="complex"
+                src= {props.blog.img}
+                onClick={(e) => {
+                  e.preventDefault();
+                  // window.location.href = `${props.blog.link}`
+                  window.open(
+                    `${props.blog.link}`,
+                    '_blank' // <- This is what makes it open in a new window.
+                  );
+                }}
+              />
+            </ButtonBase>
+          </Grid>
+          <Grid item xs={12} sm container>
+            <Grid item xs container direction="column" spacing={2}>
+              <Grid item xs>
+                <Typography gutterBottom variant="subtitle1">
+                  {props.blog.name}
+                </Typography>
+                <Typography variant="body2" gutterBottom>
+                  {props.blog.bio}
+                </Typography>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Paper>
+    </div>
+  );
+}
